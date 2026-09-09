409f0d3f4697eb3f29be715f993506d00d51be61
Ruthvik-N <sairuthvikn@gmail.com>
2026-09-09 23:09:28 +0530

    Add files via upload

---
+++ 01-basics-of-cpp/30_bitwise_shift_ops.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+
+//left shift--> '<<' works on principle of a*2 to the power b.where a << b is the syntax
+ cout << (10<<2) << endl;
+
+ //right shift--> '>>' works on principle of a/(2)^b  where a >> b is the syntax
+ cout << (8>>1) << endl;
+    return 0;
+}
