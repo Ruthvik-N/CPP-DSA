a462818c381770848caa8cde327d5c9a4e46dc27
Ruthvik-N <sairuthvikn@gmail.com>
2026-09-10 21:54:05 +0530

    Update 01_array_basics.cpp

--- 02-core-dsa/01_array_basics.cpp
+++ 02-core-dsa/01_array_basics.cpp
@@ -1,1 +1,20 @@
+# include <iostream>
+using namespace std;
 
+int main(){
+   int size = 8;
+   int ages[size];
+
+   for (int i = 0; i < size; i++)
+   {
+    cout << " enter your marks " ;
+    cin >> ages[i] ;
+   }
+   
+
+ for (int i = 0; i < size; i++)
+ {
+   cout << ages[i] << "\n"; 
+ }
+    
+}
