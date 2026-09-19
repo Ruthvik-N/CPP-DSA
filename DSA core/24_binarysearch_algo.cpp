779f6307064150a755f59f6d320cec621c9d4e8d
Ruthvik-N <sairuthvikn@gmail.com>
2026-09-20 00:36:02 +0530

    Update 24_binarysearch_algo.cpp

--- DSA core/24_binarysearch_algo.cpp
+++ DSA core/24_binarysearch_algo.cpp
@@ -6,7 +6,7 @@ int binarySearch(vector<int>arr , int tar){
     int st = 0 , end =  arr.size()-1;
     while (st <= end)
     {
-        int mid = (st + end)/2;
+        int mid = st + (end - st)/2;
         if (tar > arr[mid])
         {
             st = mid + 1;
